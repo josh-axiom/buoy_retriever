9f0d3cfb20a58cdb574430bba442dee6ae07aa82
Josh Rhoades <josh@axiomdatascience.com>
2026-04-02 09:49:31 -0800

    Adding post_save signal to auto-assign privs for known TLDs

--- backend/buoy_retriever/signals.py
+++ backend/buoy_retriever/signals.py
@@ -10,4 +10,44 @@ from django.contrib.auth.models import AbstractUser
 
 logger = logging.getLogger(__name__)
 
-# TODO: Placeholder
+KNOWN_TLDS = (
+    'axiomalaska.com',
+    'axds.co',
+    'axiomdatascience.com',
+    'tetratech.com',
+    'gmri.org',
+)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def assign_privs_for_known_tlds(sender, instance: AbstractUser = None, created=False, **kwargs):
+
+    is_known_tld = False
+
+    if not isinstance( instance, AbstractUser ):
+        logger.warning( f"Unexpected instance type: {repr(instance)}" )
+        return
+
+    if not instance.pk or not instance.email:
+        logger.warning( f"Empty instance properties (pk, email): {repr(instance)}" )
+        return
+
+    for ktld in KNOWN_TLDS:
+
+        if instance.email.endswith( ktld ):
+            is_known_tld = True
+            break
+
+    if is_known_tld:
+        get_user_model().objects.filter(pk=instance.pk).update(
+            is_staff=True,
+            is_superuser=True
+        )
+        logger.info(
+            f"Assigned privileges for known top-level domain to {repr(instance)}"
+        )
+        return
+
+    logger.warning( f"Unprivileged instance created: {repr(instance)}" )
+
+    return
