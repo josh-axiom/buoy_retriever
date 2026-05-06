8482ccc682fea40a4967f92eb51ffa01b960961e
Josh Rhoades <josh@axiomdatascience.com>
2026-05-06 10:51:40 -0800

    Allow for setting Django's STATIC_ROOT via env var

--- backend/buoy_retriever/settings.py
+++ backend/buoy_retriever/settings.py
@@ -189,7 +189,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/backend/static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", default="/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
